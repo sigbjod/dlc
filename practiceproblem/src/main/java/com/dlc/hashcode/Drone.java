7511309f49e56d95fcb0bf57438cbffa1736ad14
basilboli <Agent0295>
2016-02-11 19:49:27 +0100

    sample code

---
+++ practiceproblem/src/main/java/com/dlc/hashcode/Drone.java
@@ -0,0 +1,16 @@
+package com.dlc.hashcode;
+
+/**
+ * $Id: $
+ * <p/>
+ * Author : Vasyl Vaskul (basilboli)
+ * Created: 11/02/16
+ */
+public class Drone {
+
+  int x;
+  int y;
+  int turnsCounter;
+
+
+}
---
+++ practiceproblem/src/main/java/com/dlc/hashcode/MainX.java
@@ -0,0 +1,24 @@
+package com.dlc.hashcode;
+
+/**
+ * $Id: $
+ * <p/>
+ * Author : Vasyl Vaskul (basilboli)
+ * Created: 11/02/16
+ */
+public class MainX {
+
+  int [][] droneCoordinates;
+  int [][] droneTurns;
+  int [][] productTypeWeights;
+
+  int [][] warehouseCoordinates;
+
+
+
+
+  public static void main(String[] args) {
+    // load input
+
+  }
+}
